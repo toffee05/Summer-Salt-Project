eb208270765ea26226a35d593e52a0186d9a883d
toffee05 <allenabit05@gmail.com>
2020-12-07 00:45:37 +0800

    File uploads

---
+++ htdocs/albums.js
@@ -0,0 +1,21 @@
+var navToggled = 0;
+
+function toggleNav() {
+  if (navToggled == 0) {
+    openNav();
+    navToggled = 1;
+  } else {
+    closeNav();
+    navToggled = 0;
+  }
+}
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "18%";
+  document.getElementById("main").style.marginLeft = "18%";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
